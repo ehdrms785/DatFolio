9d717ccf699d304e334e0edb117bd9f3a515d9f8
daro <ehdrms785@naver.com>
2023-09-25 20:57:37 +0900

    Updates

--- _next/static/OVe6O6bfsWjYt6MT92uaH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-20b557b93c8ce00a.js","static/chunks/636-d88bd006b4bcfe41.js","static/chunks/pages/index-ca7f43a93326524b.js"],"/_error":["static/chunks/pages/_error-053192045264b9d4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
